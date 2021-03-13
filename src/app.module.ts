044b8b395ff0005209c5d71f7d7eaf3323879c15
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2021-03-13 23:43:53 +0700

    change listen port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 //import modul
+import { ConfigModule } from './config/config.module';
 import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 //import controller
 import { AppController } from './app.controller';
@@ -8,16 +11,15 @@ import { UsersController } from './users/users.controller';
 
 //import service
 import { AppService } from './app.service';
-import { ProductsService } from './products/products.service';
-import { UsersService } from './users/users.service';
 
 //import middleware
 import { AuthMiddleware } from './global/middleware/auth.middleware';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController, UsersController],
-  providers: [AppService, ProductsService, UsersService],
+  imports: [ProductsModule, UsersModule, ConfigModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {
 	configure(consumer: MiddlewareConsumer) {
