7e82872127fe5b262e0f8d698a22906615e44876
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2021-03-17 23:24:21 +0700

    add condition token exp & get data userAccount in middleware

--- src/global/middleware/auth.middleware.ts
+++ src/global/middleware/auth.middleware.ts
@@ -1,10 +1,15 @@
 import { Injectable, NestMiddleware, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { AuthService } from '../../auth/auth.service';
+import { AuthService } from './../../auth/auth.service';
+import { AdminsService } from './../../admins/admins.service';
+import { UserType } from './../../global/enum/userType.enum';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  constructor(private readonly authService: AuthService){}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly adminsService: AdminsService
+   ){}
   async use(req: Request, res: Response, next: NextFunction) {
   	const authHeaders = req.headers.authorization;
   	req['useAuthMiddleware'] = true;
@@ -13,9 +18,27 @@ export class AuthMiddleware implements NestMiddleware {
         if (str[0] !== 'Bearer' || !str[1]) throw new BadRequestException('Invalid token.');
        	const token = str[1];
 
+         //cek expired token
+        const verify : any = await this.authService.verify(token).catch(reason => {
+          const message = reason.name == 'TokenExpiredError' ? 'Token expired' : reason.message;
+          throw new UnauthorizedException(message);
+        });
+
        	const exists = await this.authService.findByToken(token);
         if (!exists) throw new UnauthorizedException('Not authorized');
-        console.log(exists);
+
+        let userAccount;
+        switch (verify.actorModel) {
+          case UserType.ADMIN:
+            userAccount = await this.adminsService.findById(verify.actor);
+            break;
+          
+          default:
+            break;
+        }
+        req['userAccount'] = userAccount;
+        req['userType'] = verify.actorModel;
+        console.log(req);
         next();
   	}else{
       throw new UnauthorizedException('Not authorized');
