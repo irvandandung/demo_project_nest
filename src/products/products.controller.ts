7eba396ab04ff0849a9cd030dff64bfda5cbe9bb
irvandandung <irvandandung1@gmail.com>
2021-04-30 21:09:15 +0700

    [guards] - implement user type validation use guard types

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,15 +8,21 @@ import {
   HttpCode,
   Query,
   InternalServerErrorException,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { InsertProductDto } from './dto/insertProduct.dto';
 import { Product } from './interface/product.interface';
+import { UserTypesGuard } from 'src/global/guard/userTypes.guard';
+import { UserTypes } from 'src/global/decorator/userTypes.dec';
+import { UserType } from 'src/global/enum/userType.enum';
 
+@UseGuards(UserTypesGuard)
 @Controller('products')
 export class ProductsController {
   constructor(private productService: ProductsService) {}
 
+  @UserTypes(UserType.ADMIN)
   @Post()
   async create(@Body() insertProductDto: InsertProductDto) {
     const idCreate = await this.productService.insert(insertProductDto);
@@ -25,18 +31,21 @@ export class ProductsController {
     return { status: 'success', id: idCreate };
   }
 
+  @UserTypes(UserType.ADMIN)
   @Get()
   async findAll(): Promise<Product[]> {
     const response = await this.productService.findAll();
     return response;
   }
 
+  @UserTypes(UserType.ADMIN)
   @Get('byId')
   async findOne(@Query('id') id: string): Promise<Product> {
     const response = await this.productService.findOne(id);
     return response;
   }
 
+  @UserTypes(UserType.ADMIN)
   @Put()
   async update(
     @Query('id') id: string,
@@ -49,6 +58,7 @@ export class ProductsController {
     return { status: 'success', id: idUp };
   }
 
+  @UserTypes(UserType.ADMIN)
   @Delete()
   async delete(@Query('id') id: string) {
     const idDel = await this.productService.deleteById(id);
@@ -57,6 +67,7 @@ export class ProductsController {
     return { status: 'success', id: idDel };
   }
 
+  @UserTypes(UserType.ADMIN)
   @Get('amountpercategory')
   async getGroupAmountById() {
     return await this.productService.getGroupAmountByCategory();
