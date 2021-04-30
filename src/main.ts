391bdbccbfb94ba757fa986a1f096ccf4fdd2d11
irvandandung <irvandandung1@gmail.com>
2021-04-30 21:07:58 +0700

    [validation] - add global handling validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   const configService = app.get(ConfigService);
   await app.listen(process.env.PORT || configService.getInt('APP_PORT'));
 }
